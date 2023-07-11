5ad4922489cb9b726cdc008ba51b7dcf793e05ea
Piero B <pierodev21@gmail.com>
2023-07-11 15:23:04 -0500

    feat(03-counter-app): Add style a first app

--- 03-counter-app/src/FirstApp.jsx
+++ 03-counter-app/src/FirstApp.jsx
@@ -1,7 +1,12 @@
+const  newMessage = {
+  message: "Hol mundo",
+  title: "Pired"
+}
+
 export const FirstApp = () => {
   return (
     <>
-      <h1>Piero</h1>
+      <h1>{ newMessage.title}</h1>
       <p>Soy un subtitulo</p>
     </>
   )
--- 03-counter-app/src/main.jsx
+++ 03-counter-app/src/main.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { FirstApp } from './FirstApp';
 
+import './styles.css';
+
 function App() {
   return <h1>Hola mundo</h1>;
   
